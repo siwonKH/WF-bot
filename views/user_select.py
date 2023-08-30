26a7eba716573a9cd75125add8b10567f3960742
siwonKH <kanghyunkim313@gmail.com>
2023-08-31 08:58:24 +0900

    Update: wrap UserSelect with View

--- views/user_select.py
+++ views/user_select.py
@@ -3,14 +3,15 @@ import discord
 from config import MANAGER_ROLE, MEMBER_ROLE
 
 
-class RegisterUser(discord.ui.UserSelect):
+class RegisterUser(discord.ui.View):
     def __init__(self):
-        super().__init__(placeholder="선택하세요", custom_id='register_user', max_values=10)
+        super().__init__(timeout=None)
 
-    async def callback(self, interaction: discord.Interaction):
+    @discord.ui.select(cls=discord.ui.UserSelect, placeholder="선택하세요", custom_id='register_user', max_values=10)
+    async def user_select(self, interaction: discord.Interaction):
         manager_role = interaction.guild.get_role(MANAGER_ROLE)
         if manager_role not in interaction.user.roles:
-            await interaction.response.send_message(f"권한이 없습니다", ephemeral=True)
+            await interaction.response.send_message("권한이 없습니다", ephemeral=True)
             return
 
         await interaction.response.defer()
@@ -25,4 +26,4 @@ class RegisterUser(discord.ui.UserSelect):
             await member.add_roles(member_role)
             member_string += f"- {member.name}\n"
 
-        await interaction.followup.send(content=f"""**설정 인원**\n{member_string}""")
+        await interaction.followup.send(f"**설정 인원**\n{member_string}")
