d53626f8bee84a10d761608308f4277a5fb9b489
siwonKH <kanghyunkim313@gmail.com>
2023-08-31 15:49:48 +0900

    Update: added error handle

--- views/bill_letter.py
+++ views/bill_letter.py
@@ -14,8 +14,12 @@ class BillLetter(discord.ui.View):
     async def show_btn(self, interaction: discord.Interaction, button_obj: discord.ui.Button):
         member_count = len(self.client.test_guild.get_role(MEMBER_ROLE).members)
         cost = self.client.cost
+        if cost < 1:
+            await interaction.response.send_message("나중에 다시 시도해주세요", ephemeral=True)
+            return
+
         await interaction.response.edit_message(
             content="",
-            embed=embeds.get_billing_embed(cost, member_count),
+            embed=embeds.get_billing_embed(cost, member_count, self.client.date),
             view=views.ShowAccount(self.client)
         )
