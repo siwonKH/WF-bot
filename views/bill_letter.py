52c0076992a0c1942bc2e6493d2157135c00dab8
siwonKH <kanghyunkim313@gmail.com>
2023-08-31 14:29:54 +0900

    Update: get data from ram instead of file

--- views/bill_letter.py
+++ views/bill_letter.py
@@ -2,7 +2,7 @@ import discord
 
 import views
 import embeds
-from config import TOTAL_CHANNEL, MEMBER_ROLE, TEST_GUILD_ID
+from config import MEMBER_ROLE
 
 
 class BillLetter(discord.ui.View):
@@ -12,10 +12,8 @@ class BillLetter(discord.ui.View):
 
     @discord.ui.button(label="확인하기", custom_id="bill_open", style=discord.ButtonStyle.blurple)
     async def show_btn(self, interaction: discord.Interaction, button_obj: discord.ui.Button):
-        test_guild = self.client.get_guild(TEST_GUILD_ID)
-        member_count = len(test_guild.get_role(MEMBER_ROLE).members)
-        cost = test_guild.get_channel(TOTAL_CHANNEL).name.split()[1].replace(",", "")
-        cost = int(cost)
+        member_count = len(self.client.test_guild.get_role(MEMBER_ROLE).members)
+        cost = self.client.cost
         await interaction.response.edit_message(
             content="",
             embed=embeds.get_billing_embed(cost, member_count),
