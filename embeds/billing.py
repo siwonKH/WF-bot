fd9c82809707494e11651a3ac34f3ec484c4bb3e
siwonKH <kanghyunkim313@gmail.com>
2023-08-31 15:46:54 +0900

    Update: now saves date on billing.py

--- embeds/billing.py
+++ embeds/billing.py
@@ -2,17 +2,16 @@ import discord
 from datetime import datetime
 
 
-def get_billing_embed(cost: int, member_cnt: int):
-    now = datetime.now()
+def get_billing_embed(cost: int, member_cnt: int, date: datetime):
     subtotal = format(cost, ',')
     discount = str(format(round(cost / member_cnt * (member_cnt - 1) * -1, 1), ','))
     total = str(format(round(cost / member_cnt, 1), ','))
 
     embed = discord.Embed(
-        title=f"ChatGPT Plus [{now.month}월]",
+        title=f"ChatGPT Plus [{date.month}월]",
         description=f"이번 달 요금: KRW {total}",
         colour=0x00b0f4,
-        timestamp=now
+        timestamp=date
     )
     embed.add_field(
         name="",
