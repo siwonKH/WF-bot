48bd3915e89fa1f87d7918905a975eb7df4ce00d
siwonKH <kanghyunkim313@gmail.com>
2023-08-31 15:50:43 +0900

    Update: added error handle and added refresh feat

--- views/show_account.py
+++ views/show_account.py
@@ -1,6 +1,8 @@
 import discord
 
-from .bill_letter import BillLetter
+import views
+import embeds
+from config import MEMBER_ROLE
 
 
 class ShowAccount(discord.ui.View):
@@ -10,6 +12,10 @@ class ShowAccount(discord.ui.View):
 
     @discord.ui.button(label="입금 계좌", custom_id="acc_info", style=discord.ButtonStyle.gray)
     async def show_acc_btn(self, interaction: discord.Interaction, button_obj: discord.ui.Button):
+        if self.client.acc_num is None:
+            await interaction.response.send_message("나중에 다시 시도해주세요", ephemeral=True)
+            return
+
         await interaction.response.send_message(
             content=f"{self.client.acc_bank} {self.client.acc_num}\n{self.client.acc_holder}",
             ephemeral=True
@@ -17,15 +23,25 @@ class ShowAccount(discord.ui.View):
 
     @discord.ui.button(label="계좌 복사", custom_id="acc_num", style=discord.ButtonStyle.gray)
     async def show_acc_num_btn(self, interaction: discord.Interaction, button_obj: discord.ui.Button):
+        if self.client.acc_num is None:
+            await interaction.response.send_message("나중에 다시 시도해주세요", ephemeral=True)
+            return
+
         await interaction.response.send_message(
             content=f"{self.client.acc_num}",
             ephemeral=True
         )
 
-    @discord.ui.button(label="닫기", custom_id="bill_close", style=discord.ButtonStyle.red)
+    @discord.ui.button(label="새로고침", custom_id="bill_close", style=discord.ButtonStyle.green)
     async def show_btn(self, interaction: discord.Interaction, button_obj: discord.ui.Button):
+        cost = self.client.cost
+        if cost < 1:
+            await interaction.response.send_message("나중에 다시 시도해주세요", ephemeral=True)
+            return
+
+        member_count = len(self.client.test_guild.get_role(MEMBER_ROLE).members)
         await interaction.response.edit_message(
-            content="**이번달 ChatGPT Plus 요금 안내**",
-            embed=None,
-            view=BillLetter(self.client)
+            content="",
+            embed=embeds.get_billing_embed(cost, member_count, self.client.date),
+            view=views.ShowAccount(self.client)
         )
