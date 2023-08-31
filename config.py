3c03800a2b43ebf5c4f3cc13e43fabe787c5a9f1
siwonKH <kanghyunkim313@gmail.com>
2023-08-31 16:35:22 +0900

    Fix: typo

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TEST_GUILD_ID = int(os.getenv('TEST_GUILD'))
+TEST_GUILD_ID = int(os.getenv('TEST_GUILD_ID'))
 MANAGER_ROLE = int(os.getenv('MANAGER_ROLE'))
 MEMBER_ROLE = int(os.getenv('MEMBER_ROLE'))
 TOTAL_CHANNEL = int(os.getenv('TOTAL_CHANNEL'))
